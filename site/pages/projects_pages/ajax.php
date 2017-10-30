ee0b5ebf62c71dd9e552fe5292cc7408bb89915c
CynicalToaster <danevans485@gmail.com>
2017-10-30 21:33:39 +0000

    DML for Linux

--- site/pages/projects_pages/ajax.php
+++ site/pages/projects_pages/ajax.php
@@ -1,8 +1,8 @@
 <?
     if ($_POST['reset'] != '1')
-        $command = 'C:/Users/Dan/AppData/Local/conda/conda/envs/tensorflow/python.exe C:/GitHub/TensorFlow/tf_train.py [0] ' . $_POST['neural_result']; 
+        $command = '/home/dan/anaconda/envs/tensorflow/python.exe /GitHub/TensorFlow/tf_train.py [0] ' . $_POST['neural_result']; 
     else
-        $command = 'C:/Users/Dan/AppData/Local/conda/conda/envs/tensorflow/python.exe C:/GitHub/TensorFlow/tf_reset.py';
+        $command = '/home/dan/anaconda/envs/tensorflow/python.exe /GitHub/TensorFlow/tf_reset.py';
     $output = shell_exec($command);
     echo 'Test: ' . $output;
 ?>
