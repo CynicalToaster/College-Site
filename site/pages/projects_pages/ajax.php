78f508475d4adbe1d309b7737a532faaa4a85be5
CynicalToaster <danevans485@gmail.com>
2017-10-30 21:42:47 +0000

    Corrected for Linux

--- site/pages/projects_pages/ajax.php
+++ site/pages/projects_pages/ajax.php
@@ -1,8 +1,8 @@
 <?
     if ($_POST['reset'] != '1')
-        $command = '/home/dan/anaconda/envs/tensorflow/python.exe /GitHub/TensorFlow/tf_train.py [0] ' . $_POST['neural_result']; 
+        $command = '/home/dan/anaconda3/envs/TensorFlow/bin/python /GitHub/TensorFlow/tf_train.py [0] ' . $_POST['neural_result']; 
     else
-        $command = '/home/dan/anaconda/envs/tensorflow/python.exe /GitHub/TensorFlow/tf_reset.py';
+        $command = '/home/dan/anaconda3/envs/TensorFlow/bin/python /GitHub/TensorFlow/tf_reset.py';
     $output = shell_exec($command);
     echo 'Test: ' . $output;
 ?>
