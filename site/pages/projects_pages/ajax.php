725993322b1a1826bcc13dbc939936a1276e185d
CynicalToaster <danevans485@gmail.com>
2017-10-30 21:19:25 +0000

    DML Update

--- site/pages/projects_pages/ajax.php
+++ site/pages/projects_pages/ajax.php
@@ -1,1 +1,12 @@
-<?= 'Clicked' ?>
+<?
+    if ($_POST['reset'] != '1')
+        $command = 'C:/Users/Dan/AppData/Local/conda/conda/envs/tensorflow/python.exe C:/GitHub/TensorFlow/tf_train.py [0] ' . $_POST['neural_result']; 
+    else
+        $command = 'C:/Users/Dan/AppData/Local/conda/conda/envs/tensorflow/python.exe C:/GitHub/TensorFlow/tf_reset.py';
+    $output = shell_exec($command);
+    echo 'Test: ' . $output;
+?>
+
+<script>
+    location.reload();
+</script>
