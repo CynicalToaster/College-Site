df54297a77e3ca9114ca0a5944ab0ca11e779b0c
CynicalToaster <danevans485@gmail.com>
2017-11-08 23:36:25 +0000

    Wallpaper Page

---
+++ site/pages/nn_wallpaper/init.php
@@ -0,0 +1,11 @@
+<? 
+    class Page_NN_Wallpaper extends Page
+    {
+        public function __construct()
+        {
+            $this->url = '/nn-wallpaper';
+            $this->name = 'Neural Network Wallpaper';
+            $this->desc = 'A wallpaper';
+        }
+    }
+?>
