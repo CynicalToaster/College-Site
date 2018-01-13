0f94d1ac77073025973b40c1fbda49c327713dcf
CynicalToaster <danevans485@gmail.com>
2018-01-13 13:37:32 +0000

    Voxel Engine Prototype

---
+++ site/pages/voxel_engine_prototype/init.php
@@ -0,0 +1,11 @@
+<? 
+    class Page_Voxel_Engine_Prototype extends Page
+    {
+        public function __construct()
+        {
+            $this->url = '/projects/ve_prototype';
+            $this->name = 'Voxel Engine Prototype';
+            $this->desc = 'A javascript prototype for a voxel engine.';
+        }
+    }
+?>
