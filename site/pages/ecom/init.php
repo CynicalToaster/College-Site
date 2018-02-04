2fd8a2b1b78deaea767ed13367750fea8becacf5
CynicalToaster <danevans485@gmail.com>
2018-02-04 16:36:50 +0000

    Update

---
+++ site/pages/ecom/init.php
@@ -0,0 +1,11 @@
+<? 
+    class Page_Ecom extends Page
+    {
+        public function __construct()
+        {
+            $this->url = '/ecom';
+            $this->name = 'eCommerce';
+            $this->desc = 'eCommerce Design';
+        }
+    }
+?>
