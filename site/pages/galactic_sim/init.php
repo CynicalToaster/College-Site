ac5a1c7b6ffed2be31141f3a889685cf37ae77f3
CynicalToaster <danevans485@gmail.com>
2018-02-08 00:12:47 +0000

    Change to factory sim

---
+++ site/pages/galactic_sim/init.php
@@ -0,0 +1,11 @@
+<? 
+    class Page_Galactic_Sim extends Page
+    {
+        public function __construct()
+        {
+            $this->url = '/gsim';
+            $this->name = 'Galactic Sim';
+            $this->desc = '';
+        }
+    }
+?>
